2cba85c748ab1e961d0d796c5428019bc4c5a5c8
amanaggarwal1 <i.aggarwalaman@gmail.com>
2020-03-21 19:14:25 +0530

    Now the app tracks the angle by which bottle is rotated and displays precisely the position of player, at which bottle is pointing.

--- app/src/main/java/com/example/spinthebottle/MainActivity.java
+++ app/src/main/java/com/example/spinthebottle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.spinthebottle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -45,7 +46,23 @@ public class MainActivity extends AppCompatActivity {
         Random random = new Random();
         int spinAngle = random.nextInt(3600) + 1;
         int effectiveAngle = spinAngle % 360;
-        bottle.animate().rotation(spinAngle).setDuration(2000);
+
+        //result.animate().alpha(0).setDuration(500);
+        bottle.animate().rotation(spinAngle).setDuration(2500);
+
+        int numberOfPlayers = Integer.parseInt(players.getText().toString());
+        int segmentSize = 360 / numberOfPlayers;
+        effectiveAngle = ( effectiveAngle + (segmentSize / 2) ) % 360;
+        int position =  ( effectiveAngle / segmentSize ) + 1;
+
+        final String output = "Player " + position + "'s Turn";
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                result.setText(output);
+            }
+        }, 2500);
     }
 
 }
